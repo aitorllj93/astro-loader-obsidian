64a57be0a07b70493caa964ccd7be05ef65f6346
aitorllamas <aitorllj93@gmail.com>
2025-06-28 16:44:24 +0200

    fix images

--- packages/astro-loader-obsidian/obsidian/fields/image.ts
+++ packages/astro-loader-obsidian/obsidian/fields/image.ts
@@ -1,8 +1,13 @@
+import { dirname, relative } from 'node:path';
+
 import type { ObsidianContext } from "../../types";
 
 import { getAssetFromLink } from "../obsidianId";
 
 export const image = (image: string, context: ObsidianContext) => {
   const assetId = getAssetFromLink(image, context.assets);
-  return `./${assetId}`;
+
+  const relativePath = relative(dirname(`/${context.entry}`), `/${assetId}`);
+
+  return relativePath.startsWith('.') ? relativePath : `./${relativePath}`;
 };
